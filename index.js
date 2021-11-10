50d8afad90bf06f895c2e534feee6d5b7da2e96f
Farhan Elias <faarhaan10@gmail.com>
2021-11-10 23:56:05 +0600

    server side init

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middle weares
+app.use(cors());
+app.use(express.json());
+
+//default api's
+app.get('/', (req, res) => {
+    res.send('Databse is live');
+});
+
+app.listen(port, () => {
+    console.log('DB is running on port', port);
+});
+

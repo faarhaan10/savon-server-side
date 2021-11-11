491b33dc44681d792599974aa4aac7990fc97419
Farhan Elias <faarhaan10@gmail.com>
2021-11-12 02:12:31 +0600

    db connect to mongo,post committed

--- index.js
+++ index.js
@@ -1,11 +1,57 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 //middle weares
 app.use(cors());
 app.use(express.json());
+
+//mongodb connection tools
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.y4qnm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+// crud operations
+async function run() {
+    try {
+        await client.connect();
+        // db collections
+        const database = client.db("savon");
+        const soapCollection = database.collection("soaps");
+
+        // insert data api 
+        app.post('/soaps', async (req, res) => {
+            console.log(hitted);
+        })
+
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+console.log(uri)
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    // client.close();
+});
+
+
+
+
+
+
+
+
+
+
+
 
 //default api's
 app.get('/', (req, res) => {
@@ -16,3 +62,4 @@ app.listen(port, () => {
     console.log('DB is running on port', port);
 });
 //heroku deployed
+//server: https://savon-server-sider-api.herokuapp.com/
